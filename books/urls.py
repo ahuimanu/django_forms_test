b5a9c245e3f61a0983b3e5fa8c09bcfbabf9dbab
Jeffry Babb <ahuimanu@gmail.com>
2021-08-25 22:57:19 -0500

    updated htmx

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import create_book, create_book_form
+from .views import create_book, create_book_form, detail_book
 
 
 urlpatterns = [
     path("<int:pk>/", create_book, name="create-book"),
-    path('htmx/create-book-form/', create_book_form, name='create-book-form')    
+    path("htmx/create-book-form/", create_book_form, name="create-book-form"),
+    path("htmx/book/<pk>/", detail_book, name="detail-book"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
 from .forms import BookForm, BookFormSet
 from .models import Author, Book
 
@@ -6,15 +7,28 @@ from .models import Author, Book
 def create_book(request, pk):
     author = Author.objects.get(id=pk)
     books = Book.objects.filter(author=author)
-    formset = BookFormSet(request.POST or None)
+    # formset = BookFormSet(request.POST or None)
+    form = BookForm(request.POST or None)
 
     if request.method == "POST":
-        if formset.is_valid():
-            formset.instance = author
-            formset.save()
-            return redirect("create-book", pk=author.id)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.author = author
+            book.save()
+            return HttpResponse("success")
+        else:
+            return render(request, "partials/book_form.html", context={"form": form})
+        # if formset.is_valid():
+        #     formset.instance = author
+        #     formset.save()
+        #     return redirect("create-book", pk=author.id)
 
-    context = {"formset": formset, "author": author, "books": books}
+    context = {
+        # "formset": formset,
+        "form": form,
+        "author": author,
+        "books": books,
+    }
 
     return render(request, "create_book.html", context)
 
@@ -23,3 +37,9 @@ def create_book_form(request):
     form = BookForm()
     context = {"form": form}
     return render(request, "partials/book_form.html", context)
+
+
+def detail_book(request, pk):
+    book = get_object_or_404(Book, id=pk)
+    context = {"book": book}
+    return render(request, "partials/book_detail.html", context)
